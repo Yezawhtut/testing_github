e83f864303c5a8f7218798e94a685142328b68c2
Yezawhtut <94715172+Yezawhtut@users.noreply.github.com>
2021-11-21 23:14:43 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,13 +2,5 @@
 echo "Sending email";
 echo "Another email sent";
 
-echo "thrid line from cli";
-echo "third line from online";
 
-echo "forth line from cli";
-echo "forth line from online";
-
-
-echo "fifth line from online";
-echo "fifth line from cli";
 
